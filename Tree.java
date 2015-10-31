e0f867216c632f571ba798f05065864c523f6d6b
jahooo666 <okraskaj@ee.pw.edu.pl>
2015-10-31 16:34:01 +0100

    Stworzone jakies tam drzewo- chyba mozna nazw2ac to interfejsem

--- Tree.java
+++ Tree.java
@@ -1,5 +1,21 @@
 public class Tree {
     Node root;
 
+    public Tree(){
+        root = null;
+    }
+    public void add(int index, double value){
+        if(root!=null)
+            root.insert(index, value);
+        else root = new Node(index,value);
+    }
+    public double get(int index){
+        return  root.get(index);
+    }
+    public void printFormated(){
+        if(root!=null)
+            root.printFormated(0);
+        else System.out.println("Drzewo jest puste");
+    }
 
 }
